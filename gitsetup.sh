708f5a04cf3d4cba5c8963cb2c154200a851ad6e
Gyorgy Gal <27883675+gyogal@users.noreply.github.com>
2019-07-24 12:30:15 +0200

    Minor config changes

--- gitsetup.sh
+++ gitsetup.sh
@@ -9,3 +9,6 @@ git config --global color.diff always
 git config --global color.interactive always
 git config --global color.pager true
 git config --global color.status always
+
+git config --global alias.l 'log --pretty=format:"%C(yellow)%h%Creset%C(cyan)%C(bold)%d%Creset %C(cyan)(%cd)%Creset %C(green)%an, %cn%Creset %s"'
+git config --global alias.co checkout
