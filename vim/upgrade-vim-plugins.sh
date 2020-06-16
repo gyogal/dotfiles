93bf4a198093d5b4942f9786e3b850a0133ef61d
Gyorgy Gal <27883675+gyogal@users.noreply.github.com>
2020-06-16 09:05:01 +0200

    Vim config updates

--- vim/upgrade-vim-plugins.sh
+++ vim/upgrade-vim-plugins.sh
@@ -3,5 +3,5 @@
 PLUGINSDIR="$HOME/.vim/pack/plugins/start"
 pushd . > /dev/null
 cd "$PLUGINSDIR"
-for a in *; do cd "$a" && echo "$a" && git pull && echo && cd - > /dev/null; done
+for a in *; do cd "$a" && echo "$a" && git pull --rebase && echo && cd - > /dev/null; done
 popd > /dev/null
